6782a5c180055600fa003c6b242bca77ace76003
shantanu-git-hub-work <vsys.tec@gmail.com>
2026-06-09 22:49:12 -0400

    changed py file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World , changed for git changes check "}
 
 
 @app.get("/hello/{name}")
